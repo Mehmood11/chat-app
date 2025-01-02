0f47171e39ae61177a5deb2238c5024c92f53a8d
Mehmood Khan <khan5207403@gmail.com>
2025-01-02 23:30:12 +0500

    Auth Completed and cors middleware added in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -14,7 +15,12 @@ const PORT = process.env.PORT;
 //Middleware
 app.use(express.json());
 app.use(cookieParser());
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 //Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
