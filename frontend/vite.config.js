72e9d71535f38d02ae73e701cdebd462f6e8066b
Mehmood Khan <khan5207403@gmail.com>
2025-03-12 15:21:05 +0500

    Removed the vercel file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
-  base: '/', // Ensure this is set correctly
+  base: '/',
 });
